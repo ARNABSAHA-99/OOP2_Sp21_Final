8ff267ef893da5b10482b0043d74a60036520c28
ARNAB SAHA <arnabsaha1999@gmail.com>
2021-03-22 12:09:42 +0600

    Final Lab-2

---
+++ OOP2/Final ALL CODE/Final_Lab-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Arnab", "245", 50000);
+            Account a2 = new Account("Arnab Saha", "246", 50000);
+
+            a1.Withdraw(5000);
+            a1.Deposit(1000);
+            a2.Transfer(a2, 10000);
+            a1.ShowInfo();
+            a2.ShowInfo();
+        }
+    }
+}
---
+++ OOP2/Final ALL CODE/Lab2_StudentCourse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_StudentCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //Student
+            Student s0 = new Student("ARNAB", "111", 3.0F);
+            Student s1 = new Student("ARNAB SAHA", "112", 3.41F);
+            Student s2 = new Student("ARNAB SAHA", "113", 3.23F);
+            Student s3 = new Student("ARNAB SAHA", "114", 3.55F);
+            Student s4 = new Student("ARNAB SAHA", "115", 3.45F);
+            Student s5 = new Student("ARNAB SAHA", "116", 3.22F);
+            Student s6 = new Student("ARNAB SAHA", "117", 3.67F);
+           
+              
+            //Course
+
+            Course c1 = new Course("CSC101", "OOAD");
+            Course c2 = new Course("CSC102", "TOC");
+            Course c3 = new Course("CSC103", "OS");
+
+
+            //method calling
+            c1.AddStudent(s1, s3, s4, s5, s6);
+
+            c1.PrintStudent();
+
+            c1.RemoveStudent(s5);
+
+            Console.WriteLine("\nAfter Remove Student..................\n");
+
+            s5.PrintCourse();
+            c1.PrintStudent();
+
+            Console.WriteLine("\n.............................\n");
+
+            s2.AddCourse(c1, c2, c3);
+            s2.PrintCourse();
+
+            s2.RemoveCourse(c2);
+            Console.WriteLine("\nAfter Remove Course..................\n");
+            s2.PrintCourse();
+            c2.PrintStudent();
+        }
+    }
+}
